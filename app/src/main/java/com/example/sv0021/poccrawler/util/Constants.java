8b9586887afaa704da96664e49b802ccc3cb05e8
vitoralencar <vitor.a.siq@gmail.com>
2018-11-26 07:58:25 -0200

    Commit

--- app/src/main/java/com/example/sv0021/poccrawler/util/Constants.java
+++ app/src/main/java/com/example/sv0021/poccrawler/util/Constants.java
@@ -2,7 +2,7 @@ package com.example.sv0021.poccrawler.util;
 
 public class Constants {
 
-    public static final String BASE_API_URL = "http://172.16.46.21:3000/";
+    public static final String BASE_API_URL = "http://192.168.15.14:3000/";
 
     public static final String EXTRA_LOTERIA = "extra_loteria";
 
